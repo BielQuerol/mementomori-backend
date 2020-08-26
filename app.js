e66278b0d533e0cefd98f2d1bc1efafe96cbba91
BielQuerol <bielbes@hotmail.com>
2020-08-26 18:48:43 +0200

    heroku deploy v.3

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: [process.env.PUBLIC_DOMAIN, "https://memento-mori.herokuapp.com"],
+    origin: [process.env.PUBLIC_DOMAIN, "https://memento-mori.herokuapp.com", "http://memento-mori.herokuapp.com"],
   })
 );
 // app.use((req, res, next) => {
